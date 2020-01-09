1d5f6c685fc4335f211ab24beb4dfeb2bf275ecc
Code <code@COCTL-41301.local>
2020-01-09 14:50:32 -0800

    made button and if statement with functions

--- epic7lore2/src/app/pages/faq/faq.component.ts
+++ epic7lore2/src/app/pages/faq/faq.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq.component.scss']
 })
 export class FaqComponent implements OnInit {
-
+  // values = '';
+  flip = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  clickEvent() {
+    alert('You Have clicked me, prepare to die!!');
+
+  }
+  // onKey(value: string) {
+  //   this.values += value + ' | ';
+
+  // }
+  switch() {
+    // if (this.flip) {
+    //   this.flip = false;
+    // } else {
+    //   this.flip = true;
+    // }
+    this.flip = this.flip ? !this.flip : !this.flip;
+  }
 }
