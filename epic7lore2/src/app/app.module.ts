f795938ecc6b67055590fbe7aa084898dcad4043
Code <code@COCTL-41301.local>
2020-01-09 10:07:24 -0800

    created components

--- epic7lore2/src/app/app.module.ts
+++ epic7lore2/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { FaqComponent } from './pages/faq/faq.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutusComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule,
