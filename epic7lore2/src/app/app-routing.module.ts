be76578bab1a475cdc86db03f31c9a906682895c
Code <code@COCTL-41301.local>
2020-01-09 11:22:24 -0800

    routing

--- epic7lore2/src/app/app-routing.module.ts
+++ epic7lore2/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { FaqComponent } from './pages/faq/faq.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'faq', component: FaqComponent },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: HomeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
